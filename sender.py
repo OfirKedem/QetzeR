3af11c835b83217d547ebb07fc7b7c02e9d9da44
ariel <ariegg210@gmail.com>
2021-12-13 14:04:30 +0200

    sender works good

--- sender.py
+++ sender.py
@@ -3,7 +3,7 @@ import math
 import os
 import sys
 import qrcode
-from pathlib import Path
+
 
 class QREncoderDecoder():
     def __init__(self):
@@ -20,15 +20,16 @@ class QREncoderDecoder():
         pass
 
 
-def generate_qr_from_file(data, filename='example.txt', max_size=3*2**10):
+def generate_qr_from_file(data: bytes, filename='example.txt', max_size=2**10):
     num_chunks = math.ceil(len(data) / max_size)
     qr_images = []
-    qr_data = str(0) + json.dumps({'filename': filename, 'num_chunks': num_chunks})
+    qr_data = b'0' + json.dumps({'filename': filename, 'num_chunks': num_chunks}).encode('utf-8')
+    print(qr_data[0])
     qr_image = qrcode.make(qr_data)
     qr_images.append(qr_image)
     for i in range(num_chunks):
         chunk = data[i * max_size:(i + 1) * max_size]
-        qr_data = str(i+1) + chunk
+        qr_data = str(i+1).encode('utf-8') + chunk
         print(qr_data[:10])
         qr_image = qrcode.make(qr_data)
         qr_images.append(qr_image)
@@ -38,15 +39,14 @@ def generate_qr_from_file(data, filename='example.txt', max_size=3*2**10):
 
 
 def get_file(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'rb') as f:
         data = f.read()
     return data
 
 
 if __name__ == "__main__":
-    filename = Path(sys.argv[1])
-    data = get_file(filename)
-    imgs = generate_qr_from_file(data, filename=filename.name)
+    data = get_file(sys.argv[1])
+    imgs = generate_qr_from_file(data, filename=sys.argv[1].split('/')[-1])
 
     print(len(imgs))
     for i, img in enumerate(imgs):
