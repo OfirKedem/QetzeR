a14cfe327ec736fabc774bcd83b2d41d57030612
ariel <ariegg210@gmail.com>
2021-12-15 14:04:14 +0200

    sender supports up to 1023 qr codes now. uses zbar.

--- sender.py
+++ sender.py
@@ -4,6 +4,8 @@ import os
 import sys
 import qrcode
 import base64
+import imageio
+
 
 class QREncoderDecoder():
     def __init__(self):
@@ -20,20 +22,21 @@ class QREncoderDecoder():
         pass
 
 
-def generate_qr_from_file(data: bytes, filename='example.txt', max_size=512):
+def generate_qr_from_file(data: bytes, filename='example.txt', max_size=2 ** 10):
     num_chunks = math.ceil(len(data) / max_size)
     qr_images = []
-    qr_data =  base64.b64encode(b'0' + json.dumps({'filename': filename, 'num_chunks': num_chunks}).encode('utf-8'))
+    qr_data = base64.b64encode(
+        (0).to_bytes(1, 'little') +
+        json.dumps({'filename': filename, 'num_chunks': num_chunks}).encode('utf-8'))
     print(qr_data[0])
     qr_image = qrcode.make(qr_data)
     qr_images.append(qr_image)
     for i in range(num_chunks):
         chunk = data[i * max_size:(i + 1) * max_size]
-        qr_data = base64.b64encode(str(i+1).encode('utf-8') + chunk)
+        qr_data = base64.b64encode((i + 1).to_bytes(1, 'little') + chunk)
         print(qr_data[:10])
         qr_image = qrcode.make(qr_data)
         qr_images.append(qr_image)
-
 
     return qr_images
 
@@ -50,8 +53,13 @@ if __name__ == "__main__":
 
     print(len(imgs))
     for i, img in enumerate(imgs):
-        path = os.path.join('./QetzeR/imgs', f'{i}.png')
+        path = os.path.join('./imgs', f'{i}.png')
         img.save(path)
+
+    images = []
+    for filename in os.listdir('imgs'):
+        images.append(imageio.imread(filename))
+    imageio.mimwrite('res.gif', images)
 # # print(qr_images)
 # # for qr_image in qr_images:
 # #     qr_image
